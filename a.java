5b40eae940e0243b0923459277ad4a91871ea1e6
Tommy Vercetti <7903172+tommyvct@users.noreply.github.com>
2019-03-06 20:40:05 -0600

    finished!

--- a.java
+++ a.java
@@ -739,67 +739,6 @@ class Tree
         }
     }
 
-    /**
-     * Display the whole {@code Tree}, Depth-First Traversal Version.
-     */
-    public void displayTreeDFT() 
-    {
-        Stack globalStack = new Stack();
-        int nBlanks = 32;
-        boolean isRowEmpty = false;
-
-        globalStack.push(root);
-        System.out.println("......................................................");
-
-        while (isRowEmpty == false) 
-        {
-            Stack localStack = new Stack();
-
-            isRowEmpty = true;
-    
-            for (int j = 0; j < nBlanks; j++)
-            {
-                System.out.print(' ');
-            }
-    
-            while (globalStack.isEmpty() == false) 
-            {
-                TreeNode temp = (TreeNode) globalStack.pop();
-
-                if (temp != null) 
-                {
-                    System.out.print(temp.getData());
-                    localStack.push(temp.getLeftChild());
-                    localStack.push(temp.getRightChild());
-    
-                    if (temp.getLeftChild() != null || temp.getRightChild() != null)
-                    {
-                        isRowEmpty = false;
-                    }
-                } 
-                else 
-                {
-                    System.out.print("--");
-                    localStack.push(null);
-                    localStack.push(null);
-                }
-
-                for (int j = 0; j < nBlanks * 2 - 2; j++)
-                {
-                    System.out.print(' ');
-                }
-            } 
-
-            System.out.println();
-            nBlanks /= 2;
-            while (localStack.isEmpty() == false)
-            {
-                globalStack.push(localStack.pop());
-            }
-        } 
-        System.out.println("......................................................");
-    }
-
     public void displayTreeBFT()
     {
         Queue toPrint = new Queue();
